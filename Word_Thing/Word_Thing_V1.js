2b7995ce3a1f60e63fa79ed3a3f6a222aa5f024a
thatnickguyy <nroberts@rouic.com>
2017-06-27 14:34:46 +0100

    Aah, stuff

--- Word_Thing/Word_Thing_V1.js
+++ Word_Thing/Word_Thing_V1.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
 function check(){
 
-	var theinput = document.inputs.inputBox.value;
+	var input = document.inputs.inputBox.value;
+	var split = input.split(" ");
+	var array = [input]
 
-	document.getElementByName('thewords').InnerHTML = theinput;
+var a = [], b = [], prev;
+
+    array.sort();
+    for ( var i = 0; i < array.length; i++ ) {
+        if ( array[i] !== prev ) {
+            a.push(arr[i]);
+            b.push(1);
+        } else {
+            b[b.length-1]++;
+        }
+        prev = array[i];
+    }
+
+    console.log(a);
+    console.log(b);
 }
-});
+
+
+	document.getElementById('thewords').InnerHTML = input;
+}
+})
